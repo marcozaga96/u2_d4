f9a08c7a8a9cd26325931759e511d3a8349e033d
Marco Zagaria <marcozagaria@live.com>
2024-10-24 14:31:27 +0200

    inizio

---
+++ u2_d4/src/test/java/marcozagaria/u2_d4/U2D4ApplicationTests.java
@@ -0,0 +1,13 @@
+package marcozagaria.u2_d4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2D4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
